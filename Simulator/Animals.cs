193590d379431037f4224bbaa6cc28ad2ef62af5
biggvu <pichuemce@gmail.com>
2025-01-11 00:29:07 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -8,14 +8,52 @@ namespace Simulator
 {
     public class Animals
     {
-        public required string Description { get; init; }
-        public uint Size { get; set; } = 3;
+        private string _description = "Default"; //wartość domyślna "Default", aby uniknąć ostrzeżenia kompilatora
 
-        public string Info
+        public string Description
         {
-            get
+            get { return _description; }
+
+            set
             {
-                return $"{Description} <{Size}>";
+                //usuwanie nadmiarowych spacji na początku i na końcu
+                string trimmedDescription = value.Trim();
+
+                if (string.IsNullOrWhiteSpace(trimmedDescription))
+                {
+                    trimmedDescription = "###";
+                }
+                //minimalnie 3 znaki, brakujące są uzpełniane znakiem '#'
+                if (trimmedDescription.Length < 3)
+                {
+                    trimmedDescription = trimmedDescription.PadRight(3, '#');
+                }
+                //maksymalnie 15 znaków, przycięcie tych za długich
+                if (trimmedDescription.Length > 15)
+                {
+                    trimmedDescription = trimmedDescription.Substring(0, 15).Trim();
+                }
+                //sprawdzenie czy wciąż name ma przynajmniej 3 znaki
+                if (trimmedDescription.Length < 3)
+                {
+                    trimmedDescription = trimmedDescription.PadRight(3, '#');
+                }
+                //ustawianie pierwszej litery na wielką
+                if (char.IsLower(trimmedDescription[0]))
+                {
+                    trimmedDescription = char.ToUpper(trimmedDescription[0]) + trimmedDescription.Substring(1);
+                }
+
+                _description = trimmedDescription;
             }
         }
+
+        public uint Size { get; set; } = 3;
+
+        //właściwość do odczytu Info zwracająca opis i liczebność w formie: Dogs <3>
+        public string Info
+        {
+            get { return $"{Description} <{Size}>"; }
+        }
     }
+}
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -10,31 +10,116 @@ namespace Simulator
 {
     internal class Creature
     {
-        public string Name { get; set; }
-        public int Level { get; set; }
+       
+        private string _name = "Unknown"; //wartość domyślna "Unknown"
+        private int _level = 1; //domyślna wartość początkowa "1"
+        private bool isNameInitialized = false;
+        private bool isLevelInitialized = false;
 
-        public string Info
+        public string Name
         {
-            get 
+            get {return _name;}
+
+            set
             {
-                return $"{Name} [{Level}]";
+                //name można nadać tylko raz przy inicjacji, próba nadania kolejny raz zwraca wyjątek InvalidOperationException
+                if (isNameInitialized == true)
+                {
+                    Console.WriteLine($"Name can only be set once. Current name: {_name}");
+                    return;
+                }
+
+                //usuwanie nadmiarowych spacji na początku i na końcu
+                string trimmedName = value.Trim();
+
+                //minimalnie 3 znaki, brakujące są uzpełniane znakiem '#'
+                if (trimmedName.Length < 3)
+                {
+                    trimmedName = trimmedName.PadRight(3, '#');
+                }
+                //maksymalnie 25 znaków, przycięcie tych za długich
+                if (trimmedName.Length > 25)
+                {
+                    trimmedName = trimmedName.Substring(0, 25).Trim();
+                }
+                //sprawdzenie czy wciąż name ma przynajmniej 3 znaki
+                if (trimmedName.Length < 3)
+                {
+                    trimmedName = trimmedName.PadRight(3, '#');
+                }
+                //ustawianie pierwszej litery na wielką
+                if (char.IsLower(trimmedName[0]))
+                {
+                    trimmedName = char.ToUpper(trimmedName[0]) + trimmedName.Substring(1);
+                }
+
+                _name = trimmedName;
+                isNameInitialized = true;
+
+            }
+        }
+        public int Level
+        {
+            get {return _level;}
+
+            set
+            {
+                //level można nadać tylko raz przy inicjacji, próba nadania kolejny raz zwraca wyjątek InvalidOperationException
+                if (isLevelInitialized == true)
+                {
+                    Console.WriteLine($"Level can only be set once. Current level: {_level}");
+                    return;
+                }
+
+                //ustawienie wartości z przedziału 1-10
+                if (value < 1)
+                {
+                    value = 1;
+                }
+                if (value > 10)
+                {
+                    value = 10;
+                }
+                _level = value;
+                isLevelInitialized = true;
             }
         }
 
+        // konstruktor przyjmujący wartości początkowe dla Name i opcjonalnie dla Level (wartość domyślna 1), konstruktor ma inicjować pola poprzez ich settery
         public Creature (string name, int level = 1)
         {
             Name = name;
             Level = level;
         }
 
+        //konstruktor bezparametrowy nic nie robiący
         public Creature()
         {
 
         }
 
-        public void sayHi()
+        //właściwość do odczytu Info
+        public string Info
+        {
+            get
+            {
+                return $"{Name} [{Level}]";
+            }
+        }
+
+        //metoda SayHi()
+        public void SayHi()
         {
             Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+        }
+
+        //metoda Upgrade(), która pozwoli na awansowanie stwora o jeden poziom, ale nie powyżej 10-go.
+        public void Upgrade()
+        {
+            if (_level < 10)
+            {
+                _level++;
+            }
         }
     }
 }
