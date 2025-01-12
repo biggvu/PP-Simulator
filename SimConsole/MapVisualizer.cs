1e45f5c2fab50dd2506b30e5a67026cf1265f69c
biggvu <pichuemce@gmail.com>
2025-01-12 23:03:49 +0100

    SimConsole

---
+++ SimConsole/MapVisualizer.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Simulator;
+using Simulator.Maps;
+
+namespace SimConsole
+{
+    public class MapVisualizer
+    {
+        private readonly Map _map;
+
+        public MapVisualizer(Map map)
+        {
+            _map = map;
+        }
+
+        public void Draw()
+        {
+            Console.Clear();
+            Console.OutputEncoding = Encoding.UTF8;
+
+            int width = _map.SizeX;
+            int height = _map.SizeY;
+
+            // Rysowanie górnej krawędzi
+            Console.Write(Box.TopLeft);
+            for (int x = 0; x < width; x++)
+            {
+                Console.Write($"{Box.Horizontal}{Box.Horizontal}{Box.Horizontal}");
+                if (x < width - 1)
+                {
+                    Console.Write(Box.TopMid);
+                }
+            }
+            Console.WriteLine(Box.TopRight);
+
+            // Rysowanie wierszy mapy
+            for (int y = height - 1; y >= 0; y--)
+            {
+                // Wiersz z zawartością komórek
+                Console.Write(Box.Vertical);
+                for (int x = 0; x < width; x++)
+                {
+                    var creatures = _map is SmallMap smallMap ? smallMap.At(x, y).ToList() : new List<Creature>();
+                    char cellContent = ' ';
+                    if (creatures.Count == 1)
+                    {
+                        cellContent = creatures.First() is Orc ? 'O' : 'E';
+                    }
+                    else if (creatures.Count > 1)
+                    {
+                        cellContent = 'X';
+                    }
+
+                    Console.Write($" {cellContent} ");
+                    Console.Write(Box.Vertical);
+                }
+                Console.WriteLine();
+
+                // Rysowanie linii poziomych (siatki) między wierszami
+                if (y > 0)
+                {
+                    Console.Write(Box.MidLeft);
+                    for (int x = 0; x < width; x++)
+                    {
+                        Console.Write($"{Box.Horizontal}{Box.Horizontal}{Box.Horizontal}");
+                        if (x < width - 1)
+                        {
+                            Console.Write(Box.Cross);
+                        }
+                    }
+                    Console.WriteLine(Box.MidRight);
+                }
+            }
+
+            // Rysowanie dolnej krawędzi
+            Console.Write(Box.BottomLeft);
+            for (int x = 0; x < width; x++)
+            {
+                Console.Write($"{Box.Horizontal}{Box.Horizontal}{Box.Horizontal}");
+                if (x < width - 1)
+                {
+                    Console.Write(Box.BottomMid);
+                }
+            }
+            Console.WriteLine(Box.BottomRight);
+        }
+    }
+}
---
+++ SimConsole/Program.cs
@@ -0,0 +1,49 @@
+﻿using SimConsole;
+using Simulator;
+using Simulator.Maps;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        SmallSquareMap map = new(5);
+        List<Creature> creatures = new() { new Orc("Gorbag"), new Elf("Elandor") };
+        List<Point> points = new() { new(2, 2), new(3, 1) };
+        string moves = "dlrludl";
+
+        Simulation simulation = new(map, creatures, points, moves);
+        MapVisualizer mapVisualizer = new(simulation.Map);
+
+        int turnNumber = 1;
+
+        while (!simulation.Finished)
+        {
+            mapVisualizer.Draw();
+
+            Creature currentCreature = simulation.CurrentCreature;
+
+            string creatureInfo = currentCreature switch
+            {
+                Orc orc => $"ORC {orc.Name} [{orc.Level}][{orc.Rage}] ({orc.Position})",
+                Elf elf => $"ELF {elf.Name} [{elf.Level}][{elf.Agility}] ({elf.Position})",
+                _ => $"{currentCreature.GetType().Name.ToUpper()} {currentCreature.Name} [{currentCreature.Level}] ({currentCreature.Position})"
+            };
+
+            Console.WriteLine($"Turn {turnNumber}:");
+            Console.WriteLine(creatureInfo);
+            Console.WriteLine($"Direction: {simulation.CurrentMoveName}");
+
+            simulation.Turn();
+            Console.ReadKey();
+            turnNumber++;
+        }
+
+        mapVisualizer.Draw();
+        Console.WriteLine("Simulation finished!");
+    }
+}
