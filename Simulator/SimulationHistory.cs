3fe4e9dd019959f0367079e080fc6e74066af07d
biggvu <pichuemce@gmail.com>
2025-01-19 05:59:13 +0100

    SimWeb

--- Simulator/SimulationHistory.cs
+++ Simulator/SimulationHistory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Simulator;
 using Simulator.Maps;
 
 namespace Simulator
@@ -9,21 +10,116 @@ namespace Simulator
         private Simulation _simulation { get; }
         public int SizeX { get; }
         public int SizeY { get; }
-        public List<SimulationTurnLog> TurnLogs { get; } = [];
-        // store starting positions at index 0
+        public List<SimulationTurnLog> TurnLogs { get; } = new();
 
         public SimulationHistory(Simulation simulation)
         {
-            _simulation = simulation ??
-                throw new ArgumentNullException(nameof(simulation));
+            _simulation = simulation ?? throw new ArgumentNullException(nameof(simulation));
             SizeX = _simulation.Map.SizeX;
             SizeY = _simulation.Map.SizeY;
+
             Run();
         }
 
         private void Run()
         {
-            // implement
+            LogCurrentState(null, true, null, null);
+
+            while (!_simulation.Finished)
+            {
+                var currentMappable = _simulation.CurrentMappable;
+                var previousPosition = currentMappable?.Position;
+
+                _simulation.Turn();
+
+                var currentPosition = currentMappable?.Position;
+                LogCurrentState(currentMappable, false, previousPosition, currentPosition);
+            }
         }
+
+        private void LogCurrentState(IMappable? activeMappable, bool isInitial, Point? previousPosition, Point? currentPosition)
+        {
+            var symbols = new Dictionary<Point, char>();
+            var collisionPoints = new Dictionary<Point, int>(); // Licznik obiektów na każdej pozycji
+
+            // Iteracja po wszystkich obiektach na mapie
+            foreach (var mappable in _simulation.Mappables)
+            {
+                if (mappable.Position != null)
+                {
+                    var position = mappable.Position.Value;
+
+                    // Aktualizuj licznik dla tej pozycji
+                    if (collisionPoints.ContainsKey(position))
+                    {
+                        collisionPoints[position]++;
+                    }
+                    else
+                    {
+                        collisionPoints[position] = 1;
+                    }
+                }
+            }
+
+            // Ustawianie symboli na mapie
+            foreach (var mappable in _simulation.Mappables)
+            {
+                if (mappable.Position != null)
+                {
+                    var position = mappable.Position.Value;
+
+                    // Jeśli jest kolizja, ustaw 'X'
+                    if (collisionPoints[position] > 1)
+                    {
+                        symbols[position] = 'X';
+                    }
+                    else
+                    {
+                        // Jeśli brak kolizji, ustaw symbol obiektu
+                        symbols[position] = mappable.Symbol;
+                    }
+                }
+            }
+
+            var move = isInitial
+                ? "Start"
+                : activeMappable != null
+                    ? $"{previousPosition} → {currentPosition} ({GetDirection(previousPosition, currentPosition)})"
+                    : "None";
+
+            var log = new SimulationTurnLog(
+                mappable: activeMappable?.ToString() ?? "None",
+                move: move,
+                symbols: symbols,
+                activeMappable: activeMappable,
+                previousPosition: previousPosition,
+                currentPosition: currentPosition,
+                isInitial: isInitial
+            );
+
+            TurnLogs.Add(log);
+        }
+
+        private string GetDirection(Point? previous, Point? current)
+        {
+            if (previous == null || current == null) return "None";
+
+            var deltaX = current.Value.X - previous.Value.X;
+            var deltaY = current.Value.Y - previous.Value.Y;
+
+            return (deltaX, deltaY) switch
+            {
+                (0, > 0) => "UP",
+                (0, < 0) => "DOWN",
+                ( > 0, 0) => "RIGHT",
+                ( < 0, 0) => "LEFT",
+                ( > 0, > 0) => "UP-RIGHT",
+                ( > 0, < 0) => "DOWN-RIGHT",
+                ( < 0, > 0) => "UP-LEFT",
+                ( < 0, < 0) => "DOWN-LEFT",
+                _ => "NONE"
+            };
+        }
+
     }
 }
--- Simulator/SimulationTurnLog.cs
+++ Simulator/SimulationTurnLog.cs
@@ -1,30 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Simulator;
+using Simulator.Maps;
 
 namespace Simulator
 {
-    /// <summary>
-    /// State of map after single simulation turn.
-    /// </summary>
     public class SimulationTurnLog
     {
+        public string Mappable { get; }
+        public string Move { get; }
+        public Dictionary<Point, char> Symbols { get; }
+        public IMappable? ActiveMappable { get; }
+        public bool IsInitial { get; }
+
+        public SimulationTurnLog(string mappable, string move, Dictionary<Point, char> symbols, IMappable? activeMappable, Point? previousPosition, Point? currentPosition, bool isInitial = false)
+        {
+            Mappable = mappable;
+            Move = move;
+            Symbols = symbols;
+            ActiveMappable = activeMappable;
+            IsInitial = isInitial;
+        }
+
         /// <summary>
-        /// Text representastion of moving object in this turn.
-        /// CurrentMappable.ToString()
+        /// Generate detailed information about the move and position of the object.
         /// </summary>
-        public required string Mappable { get; init; }
-        /// <summary>
-        /// Text representation of move in this turn.
-        /// CurrentMoveName.ToString();
-        /// </summary>
-        public required string Move { get; init; }
-        /// <summary>
-        /// Dictionary of IMappable.Symbol on the map in this turn.
-        /// </summary>
-        public required Dictionary<Point, char> Symbols { get; init; }
+        public string InfoText
+        {
+            get
+            {
+                if (IsInitial)
+                    return "Pozycje startowe";
+
+                if (ActiveMappable == null)
+                    return "Brak ruchu w tej turze.";
+
+                var currentPos = ActiveMappable.Position;
+                return $"{Mappable} ⇒ {Move}";
+            }
+        }
     }
 }
